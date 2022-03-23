239893c63fe5eca0cc4c1bea6a797ac63eb07dca
Mohit Arya <mohit.arya1211@gmail.com>
2022-03-23 08:07:32 -0700

    Dynamic Content Home and About Page

--- MarketPlace Project/listings/views.py
+++ MarketPlace Project/listings/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
 # Create your views here.
 def index(request):
     listings = Listing.objects.all()
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listing = paginator.get_page(page)
+
     context =  {
-        'listings' : listings
+        'listings' : paged_listing
     }
+
+    
     return render(request, 'listings/listings.html', context)
 
 
