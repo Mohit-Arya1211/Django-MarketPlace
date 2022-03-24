56a491699750b2b8be8370b1362d3e9ee8c2104d
Mohit Arya <mohit.arya1211@gmail.com>
2022-03-24 05:54:38 -0700

    Search Function Added

--- MarketPlace Project/listings/views.py
+++ MarketPlace Project/listings/views.py
@@ -1,5 +1,7 @@
+from ast import keyword
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import bedroom_choices, price_choices, state_choices
 from .models import Listing
 
 # Create your views here.
@@ -27,4 +29,38 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    # Keywords:
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    if 'city' in request.GET:
+        keywords = request.GET['city']
+        if keywords:
+            queryset_list = queryset_list.filter(city__iexact=keywords)
+
+    if 'state' in request.GET:
+        keywords = request.GET['state']
+        if keywords:
+            queryset_list = queryset_list.filter(state__iexact=keywords)
+
+    if 'bedroom' in request.GET:
+        keywords = request.GET['bedroom']
+        if keywords:
+            queryset_list = queryset_list.filter(bedroom__lte=keywords)
+    
+    if 'price' in request.GET:
+        keywords = request.GET['price']
+        if keywords:
+            queryset_list = queryset_list.filter(price__lte=keywords)
+
+    context = {
+        'price_choices' : price_choices,
+        'bedroom_choices' : bedroom_choices,
+        'state_choices' : state_choices,
+        'listings' : queryset_list
+    }
+
+    return render(request, 'listings/search.html', context)
