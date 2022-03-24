71bfdd745655a751e0c4e14b0cd0720383b44fbe
Mohit Arya <mohit.arya1211@gmail.com>
2022-03-24 08:22:09 -0700

    Final Build v2 Bugs Fixed

--- MarketPlace Project/contacts/views.py
+++ MarketPlace Project/contacts/views.py
@@ -1,4 +1,3 @@
-from email import message
 from django.shortcuts import render, redirect
 from .models import Contact
 from django.contrib import messages
@@ -13,18 +12,17 @@ def contact(request):
         phone = request.POST['phone']
         user_id = request.POST['user_id']
         message = request.POST['message']
-        realtor_email = request.POST['realtor_email']
 
         if request.user.is_authenticated:
             user_id = request.user.id
             has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
             if has_contacted:
                 messages.error(request, "You have already made a query")
-                return redirect('/listings/' + listing_id)
+                return redirect('/listing/' + listing_id)
 
 
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
         
         messages.success(request, "Submitted Successfully")
-        return redirect('/listings/' + listing_id)
+        return redirect('/listing/' + listing_id)
