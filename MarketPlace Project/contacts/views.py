5aeba2e995c3523cf0c3b2c976bdb361655e66da
Mohit Arya <mohit.arya1211@gmail.com>
2022-03-24 08:09:51 -0700

    Contact Form  Added

---
+++ MarketPlace Project/contacts/views.py
@@ -0,0 +1,30 @@
+from email import message
+from django.shortcuts import render, redirect
+from .models import Contact
+from django.contrib import messages
+
+# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        user_id = request.POST['user_id']
+        message = request.POST['message']
+        realtor_email = request.POST['realtor_email']
+
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, "You have already made a query")
+                return redirect('/listings/' + listing_id)
+
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
+        contact.save()
+        
+        messages.success(request, "Submitted Successfully")
+        return redirect('/listings/' + listing_id)
